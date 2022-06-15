3c9e7f985a8d0efbb4068b3119e06de1ffdf1116
mitclass <class@codelikeasuperstar.com>
2022-06-15 16:16:25 -0400

    Add HTML & CSS files

---
+++ src/main/java/Controller/LoginController.java
@@ -0,0 +1,22 @@
+package Controller;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "You are login successfully";
+    }
+
+    @GetMapping("/logout")
+    public String logout(){
+        return "You are logout successfully";
+    }
+
+
+}
