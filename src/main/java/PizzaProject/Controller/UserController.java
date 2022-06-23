65790a19210e344229694bc31a701b4360142343
sgirmac1 <lwselamawit.g@gmail.com>
2022-06-23 11:27:56 -0400

    Update UserController.java
    usercontroller

--- src/main/java/PizzaProject/Controller/UserController.java
+++ src/main/java/PizzaProject/Controller/UserController.java
@@ -1,4 +1,21 @@
 package PizzaProject.Controller;
 
+import PizzaProject.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("users/all")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
 }
